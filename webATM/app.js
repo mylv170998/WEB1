796a045593a745107e480b34d9ac1a1b7a52eb0e
mylv170998 <52785713+mylv170998@users.noreply.github.com>
2019-11-26 00:47:10 +0700

    giang doi

--- webATM/app.js
+++ webATM/app.js
@@ -1,25 +1,43 @@
-const express=require("express")
-const handlebars=require("express-handlebars")
-let app=express()
+const express = require("express")
+const handlebars = require("express-handlebars")
+const fs = require("fs")
+const bodyParser = require('body-parser')
+const path = require("path")
+let app = express()
+app.use((bodyParser.urlencoded({ extended: false })))
 app.engine("handlebars", handlebars({ defaultLayout: "main" }))
 app.set("view engine", "handlebars")
-app.get("/login",(req,res)=>{
-    res.render("login")
-})
-const homeRouter=require("./routers/homeRouter")
-const loginRouter=require("./routers/loginRouter")
-app.use("/",homeRouter)
-// app.use("/login",loginRouter)
-const mongoose =require("mongoose")
 
-
-mongoose.connect("mongodb://localhost/webATM",{useNewUrlParser:true},(err)=>{
-  if (err) {console.log(err)}
-  else {console.log("db connected")}
+app.get("/login", (req, res) => {
+  res.render("login")
 })
 
 
-app.listen(5000,(err)=>{
-    if (err) {console.log(err)}
-    else {console.log("app listen at 5000!")}
+app.post("/login", (req, res) => {
+  let username = req.body.user
+  let password = req.body.password
+  fs.readFile('dangnhap.json', 'utf-8', (err, docs) => {
+    if (err) { console.log(err) }
+    else {
+      let objData = JSON.parse(docs)
+      console.log(docs)
+      console.log(username,password)
+      if (username == objData.userName && password == objData.passWord) {
+        res.render("main")
+      }
+      else {
+        res.render("login", { loginStatus: "Đăng nhập thất bại" })
+      }
+    }
+  })
+})
+
+
+
+
+
+
+app.listen(5000, (err) => {
+  if (err) { console.log(err) }
+  else { console.log("app listen at 5000!") }
 })
--- webATM/routers/homeRouter.js
+++
@@ -1,8 +0,0 @@
-const express = require("express")
-const Router = express.Router()
-Router.get("/",(req,res)=>{
-    res.render("login")
-})
-
-
-module.exports = Router
